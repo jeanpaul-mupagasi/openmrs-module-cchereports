f2a6189b2670132235533fc822ee0a6a7c3415cb
Bailly RURANGIRWA <bailly.rurangirwa@gtbank.com>
2017-10-07 09:41:07 +0200

    Fixing import errors

--- api/src/test/java/org/openmrs/module/cchereports/dao/CCHEReportsDaoTest.java
+++ api/src/test/java/org/openmrs/module/cchereports/dao/CCHEReportsDaoTest.java
@@ -7,13 +7,14 @@
  * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
  * graphic logo is a trademark of OpenMRS Inc.
  */
-package org.openmrs.module.cchereports.api.dao;
+package org.openmrs.module.cchereports.dao;
 
 import org.junit.Test;
 import org.junit.Ignore;
 import org.openmrs.api.UserService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.cchereports.Item;
+import org.openmrs.module.cchereports.api.dao.CCHEReportsDao;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.hamcrest.Matchers.*;
